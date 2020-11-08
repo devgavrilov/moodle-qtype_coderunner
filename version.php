8ab880f4090d0aa8b949d37ecc857ec72852d413
Ilya Gavrilov <dev.gavrilov@gmail.com>
2020-11-08 23:16:22 +0300

    Updated version.

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2020081101;
+$plugin->version  = 2020110801;
 $plugin->requires = 2015051200;
 $plugin->cron = 0;
 $plugin->component = 'qtype_coderunner';
